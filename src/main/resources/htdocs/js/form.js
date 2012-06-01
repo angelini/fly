3b08062b45fc9e393843a0d980011f115dc27098
Alex Angelini <alex.louis.angelini@gmail.com>
2012-05-31 20:17:54 -0400

    ADDED - Client side validation

--- src/main/resources/htdocs/js/form.js
+++ src/main/resources/htdocs/js/form.js
@@ -12,6 +12,7 @@
     , initialize: function(options) {
         var $node = options.node;
         
+        this.set('validate', $node.data('validate'));
         this.set('bind', $node.data('bind'));
         this.set('url', $node.data('url'));
       }
@@ -23,28 +24,76 @@
       }
   
     , submit: function(ev) {
-         ev.preventDefault();
-         
-         var data = {};
-         
-         _.each(this.$el.find('input'), function(input) {
-           var $input = $(input)
-             , name = $input.attr('name')
-             ;
+        ev.preventDefault();
+        
+        // Remove old warnings
+        this.$el.find('.alert').remove();
+        
+        var data = {};
+        
+        _.each(this.$el.find('input'), function(input) {
+          var $input = $(input)
+            , name = $input.attr('name')
+            ;
            
-           if (name) { data[name] = $input.val(); }
-         });
-         
-         this.model.submit(data);
+          if (name) { data[name] = $input.val(); }
+        });
+        
+        this.model.submit(data);
       }
   
     , render: function() {
+        if (this.model.get('validate')) {
+          var that = this
+            , validation = []
+            , name = this.$el.attr('name')
+            ;
+          
+          if (!name) { 
+            throw new Error('Form requires "name" attribute for validation');
+          }
+          
+          _.each(this.$el.find('input'), function(input) {
+            var $input = $(input)
+              , rules = $input.data('rules')
+              , name = $input.attr('name')
+              ;
+            
+            if (!rules) { return; }
+            
+            if (!name) {
+              throw new Error('Form requires "name" attribute for validation');
+            }
+            
+            validation.push({
+                name: name
+              , rules: rules
+            });
+          });
+          
+          this.$el.unbind('submit');       
+          
+          new FormValidator(name, validation, function(errors, ev) {
+            if (errors.length == 0) {
+              that.submit(ev);
+              return;
+            }
+            
+            var alertHtml = Mustache.render(that.alertTmpl, {errors: errors});
+            
+            that.$el.append(alertHtml);
+            that.$el.find('.alert').alert();
+          });
+        
+        }
+      
         return this;
       }
   
     , initialize: function() {
         _.bindAll(this);
         this.$el = this.model.get('node');
+        this.alertTmpl = $('#form-alert-tmpl').html();
       }
   });
   
